cce0f6863c7473f3537a042f8fa912e20ec7be01
Don Browning <don.browning@gmail.com>
2021-04-20 10:22:31 -0400

    Initial Commit

---
+++ AudioSwitcher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AudioSwitcher
+//
+//  Created by Don Browning on 4/20/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AudioSwitcher/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AudioSwitcher
+//
+//  Created by Don Browning on 4/20/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
